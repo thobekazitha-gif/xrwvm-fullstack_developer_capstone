ae4af7e7ad35138b37d0aa8cf698e39ac318f0b9
thobekazitha-gif <thobekazitha40@gmail.com>
2026-01-21 07:49:01 -0500

    Containerizing Application

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/djangoapp/get_dealers" element={<Dealers/>} />
     <Route path="/dealer/:id" element={<Dealer/>} />  
     <Route path="/postreview/:id" element={<PostReview/>} />  
     </Routes>
